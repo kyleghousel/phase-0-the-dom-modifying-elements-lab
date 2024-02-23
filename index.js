abd6db41592e8bd515a62e2a0c6b5c4f8032451d
Kyle <kylegranthousel@gmail.com>
2024-02-23 17:12:22 -0500

    Submission for Creating and Inserting DOM Nodes Lab. Kyle is a champion.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainID = document.getElementById("main");
+mainID.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Kyle is the champion"
+document.body.append(newHeader);
